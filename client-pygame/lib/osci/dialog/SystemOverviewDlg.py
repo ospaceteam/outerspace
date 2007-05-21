6755e0f78600257a55a3fea0d6d9faed4502e288
neym <neym@9ec6d687-074c-4f17-8882-0daec8a15dc2>
2007-05-21 04:03:03 +0000

    Made numerous improvements to System Overview dialog, including better selection of owner/colonizable and information on stored en and bio.
    git-svn-id: svn://svn.code.sf.net/p/ospace/code/branches/ospace_0.5@142 9ec6d687-074c-4f17-8882-0daec8a15dc2

--- client-pygame/lib/osci/dialog/SystemOverviewDlg.py
+++ client-pygame/lib/osci/dialog/SystemOverviewDlg.py
@@ -34,6 +34,7 @@ class SystemOverviewDlg:
 		self.showColonizable = 0
 		self.showOtherPlayers = 0
 		self.showUncolonizable = 0
+		self.showProblems = 0
 		self.createUI()
 
 	def display(self):
@@ -62,28 +63,41 @@ class SystemOverviewDlg:
 				continue
 			system = client.get(systemID, noUpdate = 1)
 			if hasattr(system, 'planets'):
-				owned = False
-				planetsYours = 0
+				planetsMine = 0
 				planetsOwned = 0
 				planetsUnowned = 0
+				planetsGA = 0
+				planetsNotMine = 0
 				en = 0
 				bio = 0
+				enres = 0
+				biores = 0
 				stratRes = SR_NONE
 				refuelMax = 0
 				refuelInc = 0
 				upgradeShip = 0
 				repairShip = 0
 				speedBoost = 0
+				useOwner = OID_NONE
 				for planetID in system.planets:
 					planet = client.get(planetID, noUpdate = 1)
 					owner = getattr(planet, 'owner', OID_NONE)
-					if owner == player.oid:
-						owned = True
-						planetsYours += 1
+					if owner != OID_NONE:
+						useOwner = owner
+						if owner == player.oid:
+							planetsMine += 1
+						else:
+							planetsOwned += 1
+							if self.showOtherPlayers:
+								planetsNotMine += 1
 						if hasattr(planet, 'changeEn'):
 							en += planet.changeEn
 						if hasattr(planet, "changeBio"):
 							bio += planet.changeBio
+						if hasattr(planet, 'storEn'):
+							enres += planet.storEn
+						if hasattr(planet, "storBio"):
+							biores += planet.storBio
 						if hasattr(planet, "plStratRes") and planet.plStratRes != SR_NONE:
 							stratRes = gdata.stratRes[planet.plStratRes]
 						if hasattr(planet, "refuelMax"):
@@ -95,46 +109,60 @@ class SystemOverviewDlg:
 						if hasattr(planet, "fleetSpeedBoost"):
 							speedBoost = max(speedBoost, planet.fleetSpeedBoost)
 					else:
-						if owner == OID_NONE:
-							if not(hasattr(planet, "plType") and planet.plType in ("A", "G")) or self.showUncolonizable: #ignore planets that need conversion, normally
-								planetsUnowned += 1
+						if hasattr(planet, "plType") and planet.plType in ("A", "G"):
+							planetsGA += 1
+							if self.showUncolonizable:
+								planetsNotMine += 1
 						else:
-							planetsOwned += 1
-				if owned:
+							planetsUnowned += 1
+							if self.showColonizable:
+								planetsNotMine += 1
+				if planetsMine == 0: #fix no-data systems
+					en = '?'
+					bio = '?'
+					enres = '?'
+					biores = '?'
+				if (planetsMine and self.showMine) or (planetsOwned and self.showOtherPlayers) or (planetsUnowned and self.showColonizable) or (planetsGA and self.showUncolonizable):
 					if stratRes == SR_NONE:
 						stratRes = ''
 					problem = (bio < 0 or en < 0)
-					if planetsYours > 0: #make sure you own it
-						owner = player.oid
+					if planetsMine > 0: #make sure you own it
+						useOwner = player.oid
 					if speedBoost > 1:
 						speedBoost = (int) ((speedBoost-1)*100)
 					else:
 						speedBoost = ''
+					if self.showProblems:
+						color = res.getSystemOverviewProblemColor(useOwner, problem)
+					else:
+						color = res.getPlayerColor(useOwner)
 					item = ui.Item(
 						getattr(system, 'name', res.getUnknownName()),
-						tSyPnum = planetsYours+planetsOwned+planetsUnowned,
-						tSyPYnum = planetsYours,
-						tSyPOnum = planetsOwned,
-						tSyPUnum = planetsUnowned,
+						tSyPnum = planetsMine+planetsOwned+planetsUnowned+planetsGA,
+                                                tSyPTnum = planetsNotMine,
+						tSyPYnum = planetsMine,
+						tSyBioRes = biores,
+						tSyEnRes = enres,
 						tSyBio = bio,
 						tSyEn = en,
 						tSyRefuel = refuelInc,
 						tSyRefuelMax = refuelMax,
 						tSyRepair = (repairShip*100),
-						tSyUpgrade = upgradeShip,
+						tSyUpgrade = int(upgradeShip), #return an integer version
 						tSyGate = speedBoost,
                                                 tStRes = _(stratRes),
                                                 tSysID = systemID,
-						foreground = res.getSystemOverviewProblemColor(owner, problem),
+						foreground = color,
 					)
 					items.append(item)
 		self.win.vPlanets.items = items
 		self.win.vPlanets.itemsChanged()
 		# buttons
-		#self.win.vMine.pressed = self.showMine
-		#self.win.vOtherPlayers = self.showOtherPlayers
-		#self.win.vColonizable = self.showColonizable
+		self.win.vMine.pressed = self.showMine
+		self.win.vOtherPlayers = self.showOtherPlayers
+		self.win.vColonizable = self.showColonizable
 		self.win.vUncolonizable = self.showUncolonizable
+		self.win.vProblems = self.showUncolonizable
 
 	def onSelectPlanet(self, widget, action, data):
 		item = self.win.vPlanets.selection[0]
@@ -182,29 +210,32 @@ class SystemOverviewDlg:
 		)
 		self.win.subscribeAction('*', self)
 		# playets listbox
-		ui.Listbox(self.win, layout = (0, 0, 40, 26), id = 'vPlanets',
-			columns = [(_('System'), 'text', 7.5, ui.ALIGN_W),
-			(_('#Pl'), 'tSyPnum', 2, ui.ALIGN_W),
-			(_('Yours'), 'tSyPYnum', 2, ui.ALIGN_W),
-			(_('Other'), 'tSyPOnum', 2, ui.ALIGN_W),
-			(_('Colonize'), 'tSyPUnum', 2.5, ui.ALIGN_W),
-			(_('Sys Bio'), 'tSyBio', 2.5, ui.ALIGN_E),
-			(_('Sys En'), 'tSyEn', 2.5, ui.ALIGN_E),
-			(_('Refuel %'), 'tSyRefuel', 3, ui.ALIGN_E),
-			(_('Max %'), 'tSyRefuelMax', 2.5, ui.ALIGN_E),
-			(_('Repair %'), 'tSyRepair', 3, ui.ALIGN_E),
+		ui.Listbox(self.win, layout = (0, 0, 41, 26), id = 'vPlanets',
+			columns = [(_('System'), 'text', 6, ui.ALIGN_W),
+			(_('# Pl'), 'tSyPnum', 2, ui.ALIGN_E),
+			(_('Mine'), 'tSyPYnum', 2, ui.ALIGN_E),
+			(_('Other'), 'tSyPTnum', 2, ui.ALIGN_E),
+			(_('Bio'), 'tSyBioRes', 3, ui.ALIGN_E),
+			(_('Bio+-'), 'tSyBio', 2, ui.ALIGN_E),
+			(_('En'), 'tSyEnRes', 3, ui.ALIGN_E),
+			(_('En+-'), 'tSyEn', 2, ui.ALIGN_E),
+			(_('%Fuel'), 'tSyRefuel', 2.5, ui.ALIGN_E),
+			(_('%Max'), 'tSyRefuelMax', 2.5, ui.ALIGN_E),
+			(_('%Repair'), 'tSyRepair', 3, ui.ALIGN_E),
 			(_('Upgrade'), 'tSyUpgrade', 3, ui.ALIGN_E),
-			(_('Gate +%'), 'tSyGate', 3, ui.ALIGN_E),
-			(_('Strat Res'), 'tStRes', 4.5, ui.ALIGN_E)],
+			(_('+Gate %'), 'tSyGate', 3, ui.ALIGN_E),
+			(_('Strat Res'), 'tStRes', 4, ui.ALIGN_E)],
 			columnLabels = 1, action = 'onSelectPlanet', rmbAction = "onShowLocation")
-		#ui.Button(self.win, layout = (0, 26, 5, 1), text = _('My planets'), id = "vMine",
-		#	toggle = 1,	action = "onToggleCondition", data = "showMine")
-		#ui.Button(self.win, layout = (5, 26, 5, 1), text = _('Other cmdrs'), id = "vOtherPlayers",
-		#	toggle = 1,	action = "onToggleCondition", data = "showOtherPlayers")
-		#ui.Button(self.win, layout = (10, 26, 5, 1), text = _('Colonizable'), id = "vColonizable",
-		#	toggle = 1,	action = "onToggleCondition", data = "showColonizable")
-		ui.Button(self.win, layout = (0, 26, 15, 1), text = _('Include Gas Giants and Asteroids'), id = "vUncolonizable",
+		ui.Button(self.win, layout = (0, 26, 5, 1), text = _('My planets'), id = "vMine",
+			toggle = 1,	action = "onToggleCondition", data = "showMine")
+		ui.Button(self.win, layout = (5, 26, 5, 1), text = _('Other cmdrs'), id = "vOtherPlayers",
+			toggle = 1,	action = "onToggleCondition", data = "showOtherPlayers")
+		ui.Button(self.win, layout = (10, 26, 5, 1), text = _('Colonizable'), id = "vColonizable",
+			toggle = 1,	action = "onToggleCondition", data = "showColonizable")
+		ui.Button(self.win, layout = (15, 26, 5, 1), text = _('Uncolonizable'), id = "vUncolonizable",
 			toggle = 1,	action = "onToggleCondition", data = "showUncolonizable")
+		ui.Button(self.win, layout = (20, 26, 5, 1), text = _('Show Problems'), id = "vProblems",
+			toggle = 1,	action = "onToggleCondition", data = "showProblems")
 		# status bar + submit/cancel
 		ui.TitleButton(self.win, layout = (35, 27, 5, 1), text = _('Close'), action = 'onClose')
 		ui.Title(self.win, id = 'vStatusBar', layout = (0, 27, 35, 1), align = ui.ALIGN_W)
