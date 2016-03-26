e7691cfad3d7f1363bb25cf5fd429a109c6b50f4
Marek Haicman <dahaic@gmail.com>
2016-03-26 23:48:37 +0100

    Removal of files related to Windows packaging
    Reason for this removal is state of the project, which does not justify
    catering for casual players, whom the packaging is primarily targetting.

--- tools/techtree.bat
+++
@@ -1,3 +0,0 @@
-techtree.py
-start /w ..\..\tools\IView\i_view32.exe techtree.bmp /bpp=4 /convert=techtree.png
-copy techtree.png ..\..\ospace.net\website\techtree.png
--- updater/src/bccenv.bat
+++
@@ -1,3 +0,0 @@
-set PATH=%PATH%;c:\bcc55\bin
-set WXWIN=d:\wx231
-set BCCDIR=c:\bcc55
