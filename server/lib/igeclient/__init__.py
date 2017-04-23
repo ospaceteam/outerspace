18e8a4a95c99d6699e72755b5b19182030549b6a
Marek Haicman <dahaic@gmail.com>
2017-04-23 15:15:53 +0200

    Remove IGE - prefix from license headers

--- server/lib/igeclient/__init__.py
+++ server/lib/igeclient/__init__.py
@@ -3,17 +3,17 @@
 #
 #  This file is part of Outer Space.
 #
-#  IGE - Outer Space is free software; you can redistribute it and/or modify
+#  Outer Space is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
 #  the Free Software Foundation; either version 2 of the License, or
 #  (at your option) any later version.
 #
-#  IGE - Outer Space is distributed in the hope that it will be useful,
+#  Outer Space is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU General Public License for more details.
 #
 #  You should have received a copy of the GNU General Public License
-#  along with IGE - Outer Space; if not, write to the Free Software
+#  along with Outer Space; if not, write to the Free Software
 #  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #
