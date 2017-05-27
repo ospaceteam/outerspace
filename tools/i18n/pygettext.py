c96461a10c7bad1d34748a60caa4877abcaecd9c
Marek Haicman <dahaic@gmail.com>
2017-05-28 00:16:56 +0200

    Update of i18n tools

--- tools/i18n/pygettext.py
+++ tools/i18n/pygettext.py
@@ -1,4 +1,5 @@
-#! /usr/bin/env python
+#! /usr/bin/env python2
+# -*- coding: utf-8 -*-
 # Originally written by Barry Warsaw <barry@zope.com>
 #
 # Minimally patched to make it even more xgettext compatible
