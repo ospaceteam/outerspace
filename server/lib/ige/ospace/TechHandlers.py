b326fa855c6b2e39d1b2e2709a0fd9cb8e1be6d1
Marek Haicman <dahaic@gmail.com>
2018-07-03 23:26:23 +0200

    Fix spore colony deployment.

--- server/lib/ige/ospace/TechHandlers.py
+++ server/lib/ige/ospace/TechHandlers.py
@@ -52,7 +52,7 @@ def finishStructSPORECOLONY(tran, source, target, tech):
 
     if target.plSlots > 1:
         for i in range(len(target.slots),target.plSlots-1):
-            target.slots.insert(0, Utils.newStructure(tran, 9013, source.owner, hpRatio = structFromShipHpRatio))
+            target.slots.insert(0, Utils.newStructure(tran, 9013, source.owner, hpRatio = Rules.structFromShipHpRatio))
 
 def finishStructGOVCENTER(tran, source, target, tech):
     player = tran.db[source.owner]
